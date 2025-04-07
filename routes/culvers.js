5c0da30adcc89a94bf1ef85d71214bb631fb6c43
Josh <jvader.megamind@gmail.com>
2025-04-07 14:59:31 -0500

    Culvers API for multiple locations

--- routes/culvers.js
+++ routes/culvers.js
@@ -44,24 +44,55 @@ async function scrape(url, selectors) {
     return data;
 }
 
+function getLocationData(array) {
+    let data = {};
+    let dates = array[0].slice(-5);
+    let flavors = [];
+    let index = array[1].indexOf("");
+    while (index !== -1) {
+        flavors.push(array[1][index + 1]);
+        index = array[1].indexOf("", index + 1);
+    }
+    for (let i = 0; i < flavors.length; i++) {
+        data[dates[i]] = flavors[i];
+    }
+    return data;
+}
+
+// culvers/schedule?locations=loc-1,loc-2
+router.get('/schedule', async (req, res) => {
+    const locations = req.query.locations.split(',');
+    const selectors = ['h3', 'a'];
+    const scheduleData = {};
+
+    try {
+        // Use Promise.all to wait for all scrape operations to finish
+        await Promise.all(locations.map(async (element) => {
+            const url = 'https://www.culvers.com/restaurants/' + element + '?tab=current';
+            try {
+                const data = await scrape(url, selectors);
+                scheduleData[element] = getLocationData(data);
+            } catch (error) {
+                console.error(error);
+                throw error; // rethrow to be handled by the outer catch
+            }
+        }));
+
+        // After all data has been scraped, send the response
+        res.send(scheduleData);
+    } catch (error) {
+        // If any error happens, respond with 500 status
+        res.sendStatus(500);
+    }
+});
+
+// culvers/loc-1
 router.get('/:location', async (req, res) => {
     const url = 'https://www.culvers.com/restaurants/' + req.params.location + '?tab=current';
     const selectors = ['h3', 'a'];
-    let location = {};
-
     scrape(url, selectors)
     .then(data => {
-        let dates = data[0].slice(-5);
-        let flavors = [];
-        let index = data[1].indexOf("");
-        while (index !== -1) {
-            flavors.push(data[1][index + 1]);
-            index = data[1].indexOf("", index + 1);
-        }
-        for (let i = 0; i < flavors.length; i++) {
-            location[dates[i]] = flavors[i];
-        }
-        res.send(location);
+        res.send(getLocationData(data));
     })
     .catch(error => {
         console.error(error);
